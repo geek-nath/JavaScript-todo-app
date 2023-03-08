d0dc3a71924d5af38e36ee3db97edef6a91916d0
geek-nath <nath.codes@gmail.com>
2023-03-08 20:07:55 +0000

    delete task bug fix

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const taskField = document.getElementById('taskField');
 const addedTaskBox = document.getElementById('addedTaskBox');
 
+
+
+let addedReminder;
+    // delete button
+    let deleteButton;
+    
+
 taskField.addEventListener('keydown', (e)=> {
     if(e.key === 'Enter') {
         addTodoFunction();
     }
 });
 
+
 function addTodoFunction() {
     // console.log(taskField.value);
 
-    const addedReminder = document.createElement('div');
+    addedReminder = document.createElement('div');
     addedReminder.className = 'added_reminders p-3 text-white position-relative';
+
     addedReminder.innerText = taskField.value;
 
     addedTaskBox.appendChild(addedReminder);
 
-    // delete button
-    const deleteButton = document.createElement('button');
+    deleteButton = document.createElement('button');
     deleteButton.className = 'btn btn-primary delete_btn position-absolute';
+    deleteButton.addEventListener('click', deleteTask);
     
     // delete trash icon
     const deleteTrashIcon = document.createElement('i');
@@ -27,5 +36,12 @@ function addTodoFunction() {
     addedReminder.appendChild(deleteButton);
     deleteButton.appendChild(deleteTrashIcon);
 
-    deleteButton.addEventListener('click', deleteTask);
+    taskField.value = '';
+}
+
+// delete task function
+
+function deleteTask() {
+    let parentEl = this.parentElement;
+    parentEl.style.display = 'none';
 }
